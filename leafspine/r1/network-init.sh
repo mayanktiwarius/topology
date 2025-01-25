ea4e5a9b9c63a17e203c5476151cd810cbcecabc
Mayank Tiwari <mayank_tiwari@cable.comcast.com>
2025-01-25 11:01:33 -0500

    Initial change

--- leafspine/r1/network-init.sh
+++ leafspine/r1/network-init.sh
@@ -3,8 +3,8 @@
 #set -e
 
 echo "setting loopback ip addresses ..."
-ip addr add 10.0.0.21/32 dev lo
-ip -6 addr add fd00::21/128 dev lo
+ip addr add 10.1.0.21/32 dev lo
+ip -6 addr add fd01::21/128 dev lo
 
 until ip link show eth1 up; do
   echo "waiting for eth1 up ..."
